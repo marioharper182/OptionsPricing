777c9368aa72c2041a48157358b5234742e08b29
unknown <marioharper182@gmail.com>
2014-11-13 15:27:24 -0700

    added gui.

---
+++ Accelerate/Main.py
@@ -0,0 +1,97 @@
+__author__ = 'Mario'
+
+import wx
+import wx.xrc, wx.html
+import sys
+
+from pnlGui import MainPanel
+
+###########################################################################
+## Class MainPanel
+###########################################################################
+
+class MainFrame ( wx.Frame ):
+
+    def __init__( self, parent ):
+        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, pos = (100,200), size = wx.Size( 500,300 ),
+                            style = wx.MAXIMIZE_BOX | wx.RESIZE_BORDER |
+	                        wx.SYSTEM_MENU | wx.CAPTION |	 wx.CLOSE_BOX | wx.TAB_TRAVERSAL )
+
+        menuBar = wx.MenuBar()
+        menu = wx.Menu()
+        m_exit = menu.Append(wx.ID_EXIT, "E&xit\tAlt-X", "Close window and exit program.")
+        self.Bind(wx.EVT_MENU, self.OnClose, m_exit)
+        menuBar.Append(menu, "&File")
+        menu = wx.Menu()
+        m_about = menu.Append(wx.ID_ABOUT, "&About", "Information about this program")
+        self.Bind(wx.EVT_MENU, self.OnAbout, m_about)
+        menuBar.Append(menu, "&Help")
+        self.SetMenuBar(menuBar)
+
+        self.statusbar = self.CreateStatusBar()
+
+        # mainpanel = wx.Panel(self)
+
+        MainPanel(self)
+
+
+
+
+    def OnClose(self, event):
+        dlg = wx.MessageDialog(self,
+            "Do you really want to close this application?",
+            "Confirm Exit", wx.OK|wx.CANCEL|wx.ICON_QUESTION)
+        result = dlg.ShowModal()
+        dlg.Destroy()
+        if result == wx.ID_OK:
+            self.Destroy()
+
+    def OnAbout(self, event):
+        dlg = AboutBox()
+        dlg.ShowModal()
+        dlg.Destroy()
+
+    def __del__( self ):
+        pass
+
+if __name__ == '__main__':
+    app = wx.App()   # Error messages go to popup window
+    top = MainFrame(None)
+    top.Show()
+    app.MainLoop()
+    # # Run the application
+    # app = wx.App(False)
+    # dlg = MainFrame(None)
+    # # dlg.ShowModal()
+    # # dlg.Destroy()
+
+
+aboutText = """<p>Sorry, there is no information about this program. It is
+running on version %(wxpy)s of <b>wxPython</b> and %(python)s of <b>Python</b>.
+See <a href="http://wiki.wxpython.org">wxPython Wiki</a></p>"""
+
+class HtmlWindow(wx.html.HtmlWindow):
+    def __init__(self, parent, id, size=(600,400)):
+        wx.html.HtmlWindow.__init__(self,parent, id, size=size)
+        if "gtk2" in wx.PlatformInfo:
+            self.SetStandardFonts()
+
+    def OnLinkClicked(self, link):
+        wx.LaunchDefaultBrowser(link.GetHref())
+
+class AboutBox(wx.Dialog):
+    def __init__(self):
+        wx.Dialog.__init__(self, None, -1, "About <<project>>",
+            style=wx.DEFAULT_DIALOG_STYLE|wx.THICK_FRAME|wx.RESIZE_BORDER|
+                wx.TAB_TRAVERSAL)
+        hwin = HtmlWindow(self, -1, size=(400,200))
+        vers = {}
+        vers["python"] = sys.version.split()[0]
+        vers["wxpy"] = wx.VERSION_STRING
+        hwin.SetPage(aboutText % vers)
+        btn = hwin.FindWindowById(wx.ID_OK)
+        irep = hwin.GetInternalRepresentation()
+        hwin.SetSize((irep.GetWidth()+25, irep.GetHeight()+10))
+        self.SetClientSize(hwin.GetSize())
+        self.CentreOnParent(wx.BOTH)
+        self.SetFocus()
---
+++ Accelerate/pnlGui.py
@@ -0,0 +1,79 @@
+__author__ = 'Mario'
+
+import wx
+import wx.xrc
+
+###########################################################################
+## Class MainPanel
+###########################################################################
+
+class MainPanel ( wx.Panel ):
+
+    def __init__( self, parent ):
+        wx.Panel.__init__ ( self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.TAB_TRAVERSAL )
+
+        txtCtrlSizer = wx.BoxSizer( wx.VERTICAL )
+
+        self.StockPrice = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        txtCtrlSizer.Add( self.StockPrice, 0, wx.ALL, 5 )
+
+        self.StockPriceText = wx.StaticText(self, -1, 'Stock Price', pos = wx.Point(125, 10))
+
+        self.OptionPrice = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        txtCtrlSizer.Add( self.OptionPrice, 0, wx.ALL, 5 )
+
+        self.OptionStrikeText = wx.StaticText(self, -1, 'Option Strike Price', pos = wx.Point(125, 42))
+
+        self.OptionYears = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        txtCtrlSizer.Add( self.OptionYears, 0, wx.ALL, 5 )
+
+        self.OptionYearsText = wx.StaticText(self, -1, 'Option Time Length', pos = wx.Point(125, 75))
+
+        self.RiskFree = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        txtCtrlSizer.Add( self.RiskFree, 0, wx.ALL, 5 )
+
+        self.RiskFreeText = wx.StaticText(self, -1, 'Risk Free Rate', pos = wx.Point(125, 110))
+
+        self.Volatility = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        txtCtrlSizer.Add( self.Volatility, 0, wx.ALL, 5 )
+
+        self.VolatilityText = wx.StaticText(self, -1, 'Input Volatility', pos = wx.Point(125, 142))
+
+        buttonSizer = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.computeButton = wx.Button( self, wx.ID_ANY, u"Compute", wx.DefaultPosition, wx.DefaultSize, 0 )
+        buttonSizer.Add( self.computeButton, 0, wx.ALL, 5 )
+
+
+        self.clearButton = wx.Button( self, wx.ID_ANY, u"Clear", wx.DefaultPosition, wx.DefaultSize, 0 )
+        buttonSizer.Add( self.clearButton, 0, wx.ALL, 5 )
+
+        ## Bindings
+        self.computeButton.Bind(wx.EVT_BUTTON, self.OnCompute)
+        self.clearButton.Bind(wx.EVT_BUTTON, self.OnClear)
+
+        txtCtrlSizer.Add( buttonSizer, 1, wx.EXPAND, 5 )
+
+
+        self.SetSizer( txtCtrlSizer )
+        self.Layout()
+
+    def OnCompute(self, event):
+        stockPrice = self.StockPrice.GetValue()
+        optionStrike = self.OptionPrice.GetValue()
+        optionYears = self.OptionYears.GetValue()
+        Riskfree = self.RiskFree.GetValue()
+        Volatility = self.Volatility.GetValue()
+        # print(stockPrice, optionStrike, optionYears, Riskfree, Volatility)
+        #
+
+    def OnClear(self, event):
+        self.StockPrice.Clear()
+        self.OptionPrice.Clear()
+        self.OptionYears.Clear()
+        self.RiskFree.Clear()
+        self.Volatility.Clear()
+        # pass
+
+    def __del__( self ):
+        pass
