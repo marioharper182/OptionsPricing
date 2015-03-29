b420f22c7670e353f86b20f14486b37545e9fd29
unknown <marioharper182@gmail.com>
2015-03-28 23:13:14 -0600

    Adding the New code for EuropeanLookback. May add the matlab code for the naive that I used as a template.

--- PythonEngine/EuropeanFixedStrike.py
+++ PythonEngine/EuropeanFixedStrike.py
@@ -1,48 +1,188 @@
 __author__ = 'Mario'
 import numpy as np
+from EuropeanGreeks import *
+from scipy.stats import norm
 
-###### Initialize Parameters ######
-K = 100
-t = 1
-S = 105
-sig = .3
-r = .03
-div = 0
-# Alpha apparently measures performance compared to the projected performance
-alpha = .69
-## Variables that I have no clue what they mean ##
-Vbar = 0
-xi = 0
-# Steps in time
-N = 10
-#Number of simulations
-M = 100
 
-## TODO: Figure out what to set dt to
-dt = 1
+class EuropeanLookback():
+    def __init__(self, strike, t, expiry, spot, sigma, rate, dividend, alpha, dt):
 
-sig2 = sig**2
-alphadt = alpha*dt
-xisdt = xi*np.sqrt(dt)
-erddt = np.exp((r-div)*dt)
-egam1 = np.exp(2*(r-div)*dt)
-egam2 = -2*erddt + 1
-eveg1 = np.exp(-alpha*dt)
-eveg2 = Vbar - Vbar*eveg1
+        # Instantiate variables
+        self.strike = strike
+        self.t = t
+        self.expiry = expiry
+        self.tau = expiry - t
+        self.spot = spot
+        self.sigma = sigma
+        self.rate = rate
+        self.dividend = dividend
+        self.alpha = alpha
+        self.dt = dt
 
-sum_CT = 0
-sum_CT2 = 0
+        self.Vbar = 0
+        self.xi = xi = 0
 
-beta1 = -.88
-beta2 = -.42
-beta3 = -.0003
+        self.N = 10
+        self.M = 100
 
-for j in range(1,M):
-    St1 = []
-    St2 = []
-    St1.append(S)
-    St2.append(S)
-    Vt = sig2
+        self.tau = tau = self.expiry-self.t
 
-    MaxSt1 = max(St1)
-    MaxSt2 = max(St2)
+        self.Delta = self.EuroDelta()
+        self.Gamma = self.EuroGamma()
+        self.Theta = self.EuroTheta(rate, strike, tau, sigma, spot)
+        self.Rho = self.EuroRho(tau, strike, rate)
+        self.Vega = self.EuroVega(tau, spot)
+
+        self.montecarloengine()
+
+
+    def montecarloengine(self):
+
+        # Calculate the parameter shorthand that we need for the control variates
+        sig2 = self.sigma**2
+        alphadt = self.alpha*self.dt
+        xisdt = self.xi*np.sqrt(self.dt)
+        erddt = np.exp((self.rate-self.dividend)*self.dt)
+        egam1 = np.exp(2*(self.rate-self.dividend)*self.dt)
+        egam2 = -2*erddt + 1
+        eveg1 = np.exp(-self.alpha*self.dt)
+        eveg2 = self.Vbar - self.Vbar*eveg1
+        # Initialize the Greeks
+
+        sum_CT = 0
+        sum_CT2 = 0
+
+        beta1 = -.88
+        beta2 = -.42
+        beta3 = -.0003
+
+        for j in range(1, self.M):
+            St1 = []
+            St2 = []
+            St1.append(self.spot)
+            St2.append(self.spot)
+            Vt = sig2
+
+            MaxSt1 = max(St1)
+            MaxSt2 = max(St2)
+            cv1, cv2, cv3 = 0,0,0
+
+            # Initialize the d1, d2 variables for use in the control variates
+            d1_St1 = EuroD1(MaxSt1, self.strike, self.rate, self.dividend, self.sigma, self.tau)
+            d1_St2 = EuroD1(MaxSt2, self.strike, self.rate, self.dividend, self.sigma, self.tau)
+            d2_St1 = EuroD2(d1_St1, self.sigma, self.tau)
+            d2_St2 = EuroD2(d1_St2, self.sigma, self.tau)
+
+            for i in range(1, self.N):
+                # These are the hedge sensitivites
+                t = (i-1)*self.dt
+                delta1 = (d1_St1)
+                delta2 = (d1_St2)
+                gamma1 = EuroGamma(d1_St1, MaxSt1, self.sigma, self.tau)
+                gamma2 = EuroGamma(d1_St2, MaxSt2, self.sigma, self.tau)
+                vega1 = EuroVega(d1_St1, self.tau, MaxSt1)
+                vega2 = EuroVega(d1_St2, self.tau, MaxSt2)
+
+                # Evolution of Variance
+                e = np.random.normal(0,1)
+                Vtn = Vt + alphadt*(self.Vbar - Vt) + xisdt*np.sqrt(Vt)*e
+
+                # Evolution of Asset Price
+                Stn1 = St1 * np.exp( (self.rate-self.dividend-.5*Vt)*self.dt + np.sqrt(Vt)*xisdt*e)
+                Stn2 = St1 * np.exp( (self.rate-self.dividend-.5*Vt)*self.dt + np.sqrt(Vt)*xisdt*-e)
+
+                # Process the Control Variates
+                cv1 = cv1 + delta1*(Stn1 - St1*erddt) + delta2*(Stn2 - St2*erddt)
+                cv2 = cv2 + gamma1*((Stn1-St1)^2 - St1^2 * (egam1*np.exp(Vt*self.dt)+egam2)) + \
+                      gamma2*((Stn2-St2)^2 - St2^2*(egam1*np.exp(Vt*self.dt)+egam2))
+                cv3 = cv3 + vega1*((Vtn-Vt)-(Vt*eveg1+eveg2-Vt)) + \
+                      vega2*((Vtn-Vt)-(Vt*eveg1+eveg2-Vt))
+
+    # def EuroDelta(self):
+    #     Delta = norm.cdf(self.d1)
+    #     return Delta
+    #
+    # def EuroGamma(self):
+    #     Gamma = norm.ppf(norm.cdf(self.d1)) / (self.spot*self.sigma*np.sqrt(self.tau))
+    #     return Gamma
+    #
+    # def EuroTheta(self, rate, strike, tau, sigma, spot):
+    #     Theta = -rate*strike*np.exp(-rate*tau) * norm.cdf(self.d2) - (sigma*spot*norm.ppf(norm.cdf(self.d1))/(2*tau))
+    #     return Theta
+    #
+    # def EuroRho(self, tau, strike, rate):
+    #     Rho = tau*strike*np.exp(-rate*tau) * norm.cdf(self.d2)
+    #     return Rho
+    #
+    # def EuroVega(self, tau, spot):
+    #     Vega = np.sqrt(tau)*spot*norm.cdf(self.d1)
+    #     return Vega
+
+    # def ReturnGreeks(self):
+    #     EuroGreeks = {}
+    #     EuroGreeks['Delta'] = self.Delta
+    #     EuroGreeks['Gamma'] = self.Gamma
+    #     EuroGreeks['Theta'] = self.Theta
+    #     EuroGreeks['Rho'] = self.Rho
+    #     EuroGreeks['Vega'] = self.Vega
+    #
+    #     return EuroGreeks
+
+
+    def SetVbar(self, Vbar):
+        self.Vbar = Vbar
+
+    def Setxi(self, xi):
+        self.xi = xi
+
+    def SetN(self, N):
+        self.N = N
+
+    def SetM(self, M):
+        self.M = M
+
+    def GetVariables(self):
+        vardic = {}
+        vardic['strike'] = self.strike
+        vardic['t'] = self.t
+        vardic['expiry'] = self.expiry
+        vardic['S'] = self.spot
+        vardic['sig'] = self.sigma
+        vardic['r'] = self.rate
+        vardic['div'] = self.dividend
+        vardic['alpha'] = self.alpha
+        vardic['Vbar'] = self.Vbar
+        vardic['xi'] = self.xi
+        vardic['N'] = self.N
+        vardic['M'] = self.M
+
+        return vardic
+
+def main():
+
+    ###### Initialize Parameters ######
+    strike = 100
+    t = 1
+    expiry = 10
+    spot = 105
+    sigma = .3
+    rate = .03
+    dividend = 0
+    # Alpha apparently measures performance compared to the projected performance
+    alpha = .69
+    ## Variables that I have no clue what they mean ##
+    Vbar = 0
+    xi = 0
+    # Steps in time
+    N = 10
+    #Number of simulations
+    M = 100
+
+    ## TODO: Figure out what to set dt to
+    dt = 1
+
+    EuropeanLookback(strike=strike, spot=spot, t=t, expiry=expiry, sigma=sigma, dividend=dividend, alpha=alpha, rate=rate, dt=dt)
+
+
+if __name__ == '__main__':
+    main()
---
+++ PythonEngine/EuropeanGreeks.py
@@ -0,0 +1,66 @@
+__author__ = 'HarperMain'
+
+from scipy.stats import norm
+import numpy as np
+
+# class EuropeanLookbackGreeks():
+#
+#     def __init__(self, spot, strike, rate, dividend, sigma, expiry, t):
+#
+#         self.spot = spot
+#         self.strike = strike
+#         self.rate = rate
+#         self.dividend = dividend
+#         self.sigma = sigma
+#         self.expiry = expiry
+#         self.t = t
+#
+#         self.tau = tau = self.expiry-self.t
+#         self.d1 = (np.log(spot/strike) + (rate - dividend + 1/2 * sigma**2)*tau) / (np.sqrt(tau))
+#         self.d2 = self.d1 - sigma * np.sqrt(tau)
+#
+#         self.Delta = self.EuroDelta()
+#         self.Gamma = self.EuroGamma()
+#         self.Theta = self.EuroTheta(rate, strike, tau, sigma, spot)
+#         self.Rho = self.EuroRho(tau, strike, rate)
+#         self.Vega = self.EuroVega(tau, spot)
+
+def EuroDelta(d1):
+    Delta = norm.cdf(d1)
+    return Delta
+
+def EuroGamma(d1, spot, sigma, tau):
+    Gamma = norm.ppf(norm.cdf(d1)) / (spot*sigma*np.sqrt(tau))
+    return Gamma
+
+def EuroTheta(d1, d2, rate, strike, tau, sigma, spot):
+    Theta = -rate*strike*np.exp(-rate*tau) * norm.cdf(d2) - \
+            (sigma*spot*norm.ppf(norm.cdf(d1))/(2*tau))
+    return Theta
+
+def EuroRho(d2, tau, strike, rate):
+    Rho = tau*strike*np.exp(-rate*tau) * norm.cdf(d2)
+    return Rho
+
+def EuroVega(d1, tau, spot):
+    Vega = np.sqrt(tau)*spot*norm.cdf(d1)
+    return Vega
+
+def EuroD1(spot, strike, rate, dividend, sigma, tau):
+    d1 = (np.log(spot/strike) + (rate - dividend + 1/2 *
+                                 sigma**2)*tau) / (np.sqrt(tau))
+    return d1
+
+def EuroD2(d1, sigma, tau):
+    d2 = d1 - sigma * np.sqrt(tau)
+    return d2
+
+    # def ReturnGreeks(self):
+    #     EuroGreeks = {}
+    #     EuroGreeks['Delta'] = self.Delta
+    #     EuroGreeks['Gamma'] = self.Gamma
+    #     EuroGreeks['Theta'] = self.Theta
+    #     EuroGreeks['Rho'] = self.Rho
+    #     EuroGreeks['Vega'] = self.Vega
+    #
+    #     return EuroGreeks
