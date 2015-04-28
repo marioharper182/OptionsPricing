98f9bd372c2c1f66cba01483e5598487eb1b0e5b
unknown <marioharper182@gmail.com>
2015-04-28 00:18:31 -0600

    Added the American Option feature to the application.

--- Gui/View/pnlAmerican.py
+++ Gui/View/pnlAmerican.py
@@ -2,7 +2,7 @@ __author__ = 'Mario'
 
 import wx
 import wx.xrc
-from Engine_European import EuropeanOption
+from Engine_American import American
 
 ###########################################################################
 ## Class MainPanel
@@ -43,12 +43,8 @@ class PanelAmerican ( wx.Panel ):
         self.Fixings = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
         txtCtrlSizer.Add(self.Fixings, 0, wx.ALL, 5)
 
-        self.FixingsText = wx.StaticText(self, -1, 'Number of Price Fixings', pos = wx.Point(125, 174))
+        self.FixingsText = wx.StaticText(self, -1, 'Number of Nodes (Fixings to branch out to)', pos = wx.Point(125, 174))
 
-        self.Iterations = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
-        txtCtrlSizer.Add(self.Iterations, 0, wx.ALL, 5)
-
-        self.IterationsText = wx.StaticText(self, -1, 'Number of Iterations', pos = wx.Point(125, 206))
 
         Choices = ['Call', 'Put']
         self.ChoiceBox = wx.Choice(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, Choices, 0)
@@ -75,19 +71,17 @@ class PanelAmerican ( wx.Panel ):
         self.Layout()
 
     def OnCompute(self, event):
-        stockPrice = self.StockPrice.GetValue()
-        optionStrike = self.OptionPrice.GetValue()
-        optionYears = self.OptionYears.GetValue()
-        Riskfree = self.Riskfree.GetValue()
-        Volatility = self.Volatility.GetValue()
-        Fixings = self.Fixings.GetValue()
-        Iter = self.Iterations.GetValue()
+        spot = self.StockPrice.GetValue()
+        strike = self.OptionPrice.GetValue()
+        expiry = self.OptionYears.GetValue()
+        rate = self.Riskfree.GetValue()
+        sigma = self.Volatility.GetValue()
+        N = self.Fixings.GetValue()
         flag = 'c' if self.ChoiceBox.GetString(self.ChoiceBox.GetCurrentSelection()) == 'Call' else 'p'
-        EuroOption = EuropeanOption(stockPrice, Riskfree, Volatility, optionYears, Fixings, Iter, optionStrike, flag)
-        EuroOption.GetPrice()
+        AmericanResult = American(flag, spot, strike, rate, sigma, expiry)
+        A = AmericanResult.GetPrice()
 
-        print( 'The MonteCarlo Price of the European Option is:', EuroOption.GetPrice()[0])
-        print( 'The associated standard deviation and standard errors are:', EuroOption.GetPrice()[1], EuroOption.GetPrice()[2])
+        print( 'The Binomial Price of the American Option is:', A)
         # print(stockPrice, optionStrike, optionYears, Riskfree, Volatility)
         #
 
@@ -98,7 +92,6 @@ class PanelAmerican ( wx.Panel ):
         self.Riskfree.Clear()
         self.Volatility.Clear()
         self.Fixings.Clear()
-        self.Iterations.Clear()
         self.ChoiceBox.Clear()
         # pass
 
